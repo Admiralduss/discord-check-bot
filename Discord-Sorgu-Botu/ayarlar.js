010d5b6afc4f4c9bd33b5ef5332a974ec3f78651
Adni <86412702+Admiralduss@users.noreply.github.com>
2023-03-14 21:11:11 +0300

    Add files via upload

---
+++ Discord-Sorgu-Botu/ayarlar.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    token: "MTA2NzkwNjc3NTgwNzk2MzIxNg.GXmrCi.vVEyxEkoc_fn3uhfzIHAJLix0-4I2ywNqws9T8",
+    prefix: ".",
+    sahip: "607848094788354048",
+    durum: "",
+
+    api: {
+        TCKN: "",
+        TC_GSM: "",
+        GSM_TC: "",
+        AD_SOYAD: "",
+        AILEA: "",
+        AILEB: "",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "",
+        SOYAD: "",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: ""
+    },
+
+    roller: {
+        admin: "",
+        vip: "",
+        premium: "",
+        booster: "",
+        limitlirol: "",
+        freemium: "",
+        yardimci: ""
+    },
+
+    log: "",
+    log2: "",
+
+    wl: [""], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "",
+    kanal: [""],
+    seskanal: "",
+
+    sorgulimit: "",
+
+    bakim: ""
+}
